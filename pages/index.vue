ffd3928701324392fafdb7fef77113a53e19ce1f
shotaro <shotaro.tu13@gmail.com>
2021-10-04 08:07:38 +0900

    set up sample

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,52 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-card class="text-card">
+  <v-col justify="center" align="center">
+    <p class="height-text">height : {{ height }}</p>
+    <div ref="text-card" class="text-card">
       <p class="text">{{ text }}</p>
-    </v-card>
-  </v-row>
+    </div>
+  </v-col>
 </template>
 <script>
 export default {
   data() {
     return {
-      text: 'hogehoge'
+      text: 'hogehoge',
+      height: 0,
     }
   },
-  mounted() {
-    setTimeout(() => {
-      this.text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    }, 2000);
+  async mounted() {
+    this.onResized()
+    // element.addEventListener('resize', callback)ではリサイズの検知がされない
+    this.$refs['text-card'].addEventListener('resize', this.onResized)
+
+    // fetch data
+    const data = await this.fetchData()
+    this.text = data
+  },
+  beforeDestroy() {
+    this.$refs['text-card'].removeEventListener('resize', this.onResized)
+  },
+  methods: {
+    onResized() {
+      this.height = this.$refs['text-card'].getBoundingClientRect().height
+    },
+    fetchData() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+        }, 2000);
+      })
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
+.height-text {
+  margin-top: 200px;
+}
 .text-card {
   width: 200px;
-  margin-top: 200px;
+  background-color: grey;
 
   .text {
     margin-bottom: 0;
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,61 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-col justify="center" align="center">
+    <p class="height-text">height : {{ height }}</p>
+    <div ref="text-card" class="text-card">
+      <p class="text">{{ text }}</p>
+    </div>
+  </v-col>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      text: 'hogehoge',
+      height: 0,
+
+      // Resize Observer Instance
+      ro: null,
+    }
+  },
+  async mounted() {
+    this.onResized()
+    this.ro = new ResizeObserver(this.onResized)
+      .observe(this.$refs['text-card'])
+
+    // fetch data
+    const data = await this.fetchData()
+    this.text = data
+  },
+  beforeDestroy() {
+    if (this.ro) {
+      this.ro.this.ro.unobserve(this.$refs['text-card'])
+    }
+  },
+  methods: {
+    onResized() {
+      this.height = this.$refs['text-card'].getBoundingClientRect().height
+    },
+    fetchData() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+        }, 2000);
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.height-text {
+  margin-top: 200px;
+}
+.text-card {
+  width: 200px;
+  background-color: grey;
+
+  .text {
+    margin-bottom: 0;
+    padding: 4px;
+  }
+}
+</style>
